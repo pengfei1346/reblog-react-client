41558a05ceadf981092199ec13bb2a5cb5b7a288
cpf <chenpengfei@aicaihr.com>
2021-01-14 15:34:50 +0800

    优化

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,33 +1,66 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import {
   Link,
   withRouter
 } from 'react-router-dom'
-import'./index.scss';
+import './index.scss';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fatherName: this.props.className
+    }
+  }
+
+  // componentDidMount() {
+  //   // console.log(this.props);
+  //   // this.setState({
+  //   //   fatherName: this.props.className
+  //   // })
+  // }
+
+  //
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    if(nextProps.className !== this.state.fatherName) {
+      this.setState({
+        fatherName: nextProps.className
+      })
+    }
+    return true
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // console.log('prevProps--', prevProps);
+    // if(prevState.className !== prevState.fatherName) {
+    //   this.setState({
+    //     fatherName: prevState.fatherName
+    //   })
+    // }
+  }
+
   render() {
-    const { pathname } = this.props.location
+    const {pathname} = this.props.location
 
     return (
-      <ul className="nav">
+      <ul className={`nav ${this.state.fatherName}`}>
         <Link to="/">
-          <li className={pathname==='/'?'active':''}>首页</li>
+          <li className={pathname === '/' ? 'active' : ''}>首页</li>
         </Link>
         <Link to="/articles">
-          <li className={pathname==='/articles'?'active':''}>文章</li>
+          <li className={pathname === '/articles' ? 'active' : ''}>文章</li>
         </Link>
         <Link to="/archive">
-          <li className={pathname==='/archive'?'active':''}>归档</li>
+          <li className={pathname === '/archive' ? 'active' : ''}>归档</li>
         </Link>
         <Link to="/collections">
-          <li className={pathname==='/collections'?'active':''}>收藏</li>
+          <li className={pathname === '/collections' ? 'active' : ''}>收藏</li>
         </Link>
         <Link to="/leavingMessage">
-          <li className={pathname==='/leavingMessage'?'active':''}>留言</li>
+          <li className={pathname === '/leavingMessage' ? 'active' : ''}>留言</li>
         </Link>
         <Link to="/about">
-          <li className={pathname==='/about'?'active':''}>关于</li>
+          <li className={pathname === '/about' ? 'active' : ''}>关于</li>
         </Link>
       </ul>
     )
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -19,22 +19,22 @@ const viewHeight = window.innerHeight || document.documentElement.clientHeight;
 // 用新的 throttle 包装 scroll 的回调
 
 // eslint-disable-next-line no-unused-vars
-const lazyload = throttle(() => {
+const lazyLoad = throttle(() => {
   // 获取所有的图片标签
-  const imgs = document.querySelectorAll('#list img');
-  console.log(imgs);
+  const iamges = document.querySelectorAll('#list img');
+  console.log(iamges);
   // num 用于统计当前显示到了哪一张图片，避免每次都从第一张图片开始检查是否露出
   let num = 0;
-  for (let i = num; i < imgs.length; i++) {
+  for (let i = num; i < iamges.length; i++) {
     // 用可视区域高度减去元素顶部距离可视区域顶部的高度
-    let distance = viewHeight - imgs[i].getBoundingClientRect().top;
+    let distance = viewHeight - iamges[i].getBoundingClientRect().top;
     // 如果可视区域高度大于等于元素顶部距离可视区域顶部的高度，说明元素露出
     if (distance >= 100) {
       // 给元素写入真实的 src，展示图片
-      let hasLaySrc = imgs[i].getAttribute('data-has-lazy-src');
+      let hasLaySrc = iamges[i].getAttribute('data-has-lazy-src');
       if (hasLaySrc === 'false') {
-        imgs[i].src = imgs[i].getAttribute('data-src');
-        imgs[i].setAttribute('data-has-lazy-src', true);
+        iamges[i].src = iamges[i].getAttribute('data-src');
+        iamges[i].setAttribute('data-has-lazy-src', true);
       }
       // 前 i 张图片已经加载完毕，下次从第 i+1 张开始检查是否露出
       num = i + 1;
@@ -55,31 +55,33 @@ class home extends Component{
       pageSize: 10,
       list: [],
       total: 0
-    }
-    this.getArticles = this.getArticles.bind(this)
+    };
+    this.getArticles = this.getArticles.bind(this);
     this.pageScroll = this.pageScroll.bind(this)
   }
 
   componentDidMount = () => {
-    window.addEventListener('scroll',this.pageScroll)
+    window.addEventListener('scroll',this.pageScroll);
     this.getArticles()
-  }
+  };
 
   componentWillUnmount = () => {
     window.removeEventListener('scroll',this.pageScroll)
-  }
+  };
 
   pageScroll() {
-    let isHidden = getScrollTop()>0
-    this.setState({
-      isHidden: !isHidden
-    });
+    let isHidden = getScrollTop() === 0;
+    if(isHidden !== this.state.isHidden) {
+      this.setState({
+        isHidden: isHidden
+      });
+    }
 
     setTimeout(() => {
       if (getScrollTop() + getWindowHeight() > getDocumentHeight() - 100) {
         // 如果不是已经没有数据了，都可以继续滚动加载
         if (this.state.isLoadEnd === false && this.state.isLoading === false) {
-          let page = this.state.page + 1
+          let page = this.state.page + 1;
           this.setState({
             page: page
           });
@@ -96,7 +98,7 @@ class home extends Component{
     let params = {
       page: this.state.page,
       pageSize: this.state.pageSize
-    }
+    };
     request.get('/article',{params:params}).then(data =>{
       this.setState(preState => ({
         list: [...preState.list, ...data.data],
@@ -120,21 +122,21 @@ class home extends Component{
         <div className={`article-left border-shadow ${i%2 !== 0? 'left' : 'right'}`}>
           <div className="detail-content">
             <div className="detail-publish">
-              <i className="iconfont icon-shijian2"></i>
+              <i className="iconfont icon-shijian2"/>
               {item.createdAt?timestampToTime(item.createdAt,true):''}
             </div>
             <div className="detail-title">{item.title}</div>
             <div className="detail-meta">
-              <div className="meta-box"><i className="iconfont icon-yanjing"></i>{item.browseNum}</div>
-              <div className="meta-box"><i className="iconfont icon-huifu"></i>{item.replyNum}</div>
-              <div className="meta-box"><i className="iconfont icon-zuozhe"></i>{item.author}</div>
+              <div className="meta-box"><i className="iconfont icon-yanjing"/>{item.browseNum}</div>
+              <div className="meta-box"><i className="iconfont icon-huifu"/>{item.replyNum}</div>
+              <div className="meta-box"><i className="iconfont icon-zuozhe"/>{item.author}</div>
             </div>
             <div className="detail-abstract">
               <p>摘要：</p>
               <p style={{textIndent: '2em'}}>{item.abstract}</p>
             </div>
             <div className="detail-edit">
-              <div><i className="iconfont icon-shenglvehao"></i></div>
+              <div><i className="iconfont icon-shenglvehao"/></div>
             </div>
           </div>
         </div>
@@ -148,9 +150,7 @@ class home extends Component{
 
     return (
       <div className="home">
-        <div className={`header ${this.state.isHidden? '': 'active'}`}>
-          <Header/>
-        </div>
+        <Header className={this.state.isHidden? 'hidden': 'show'}/>
 
         <div className="bg-box" />
 
